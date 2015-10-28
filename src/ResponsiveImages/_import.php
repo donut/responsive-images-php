fd489354125fa79d7a973af63597f5c031b7eadb
Donovan Mueller <zotobi@gmail.com>
2015-10-28 10:58:18 -0700

    Fix broken require for functional-php

--- src/ResponsiveImages/_import.php
+++ src/ResponsiveImages/_import.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/functional-php/src/Functional/_import.php';
+require_once __DIR__.'/../../vendor/functional-php/src/Functional/_import.php';
 
 require_once __DIR__.'/Slot.php';
 require_once __DIR__.'/SlotGroup.php';
