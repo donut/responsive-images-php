af0f260004e61d24768cba00142d99e2b0867f87
Donovan Mueller <zotobi@gmail.com>
2015-10-30 13:30:26 -0700

    Document Size class.

--- src/ResponsiveImages/Size.php
+++ src/ResponsiveImages/Size.php
@@ -2,33 +2,60 @@
 
 namespace ResponsiveImages;
 
+
 use Functional as F;
 
 
+/**
+ * Represents a condition of a `sizes` attribute along with the necessary image
+ * dimensions to be matched with the condition.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-sizes
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/source#attr-sizes
+ *
+ * @package ResponsiveImages
+ */
 class Size
 {
   /**
+   * The media query condition including parentheses.
+   *
    * @var string|null
    */
   private $media_query;
   public function getMediaQuery(){ return $this->media_query; }
+
   /**
-   * @var integer|null
+   * The viewport width for this size that will be used in the `sizes`
+   * attribute value. Expressed in `vw` units.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/length#Viewport-percentage_lengths
+   *
+   * @var float|null
    */
   private $viewport_width;
 
   /**
+   * The minimum width the image will take when the condition of this size is
+   * met. Expressed in pixels at a 1:1 DPI.
+   *
    * @var integer
    */
   private $min_width;
   public function getMinWidth(){ return $this->min_width; }
+
   /**
+   * The maximum width the image will take when the condition of this size is
+   * met. Expressed in pixels at a 1:1 DPI.
+   *
    * @var integer
    */
   private $max_width;
   public function getMaxWidth(){ return $this->max_width; }
 
   /**
+   * The aspect ratio of the image.
+   *
    * @var float
    */
   private $aspect_ratio;
@@ -47,10 +74,19 @@ class Size
    * Size constructor.
    *
    * @param integer[]   $range
+   *   Either `[$min_width]` or `[$min_width, $max_width]`. If only $min_width
+   *   is specified, it will be used for the $max_width value as well.
    * @param float       $aspect_ratio
+   *   The aspect ratio. Looks best if you pass it as a fraction, such as 16/9.
    * @param string|null $query
+   *   The media query to be used in the `sizes` attribute value. If null, no
+   *   condition is set.
    * @param float|null  $vw
-   * @param float|null  $ar_tolerance
+   *   The viewport width to be used in the `sizes` attribute value. If null,
+   *   $min_width will be used with with `px` units.
+   * @param float       $ar_tolerance
+   *   The aspect ratio tolerance. Used to define the minimum and maximum
+   *   acceptable aspect ratios.
    */
   function __construct(array $range, $aspect_ratio, $query=null, $vw=null
                       ,$ar_tolerance=0.0)
@@ -66,6 +102,16 @@ class Size
   }
 
 
+  /**
+   * Determines whether or not the passed aspect ratio is close enough to
+   * $this->aspect_ratio based on $this->aspect_ratio_tolerance.
+   *
+   * @param float $aspect_ratio
+   *   The aspect ratio to test.
+   *
+   * @return bool
+   *   true if it matches, false otherwise.
+   */
   public function matchesAspectRatio($aspect_ratio)
   {
     $min = $this->aspect_ratio - $this->aspect_ratio_tolerance;
@@ -74,12 +120,25 @@ class Size
     return ($min <= $aspect_ratio and $aspect_ratio <= $max);
   }
 
+  /**
+   * Renders only the width portion of the `sizes` attribute value.
+   *
+   * @return string
+   *   The width the image is expected to be at in either `vw` or `px` units.
+   */
   public function renderWidthOnly()
   {
     return (isset($this->viewport_width))
          ? "{$this->viewport_width}vw" : "{$this->min_width}px";
   }
 
+  /**
+   * Renders a full `sizes` attribute value, with condition and expected width.
+   *
+   * @return string
+   *   A value to be used in the comma separated conditions of a `sizes`
+   *   attribute.
+   */
   public function __toString()
   {
     $size = '';
